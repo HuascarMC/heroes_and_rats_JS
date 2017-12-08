cf0c65aabceb8e4d629f0cf1de6c428677b648d5
Huascar <huascarmc@hotmail.com>
2017-12-08 15:39:55 +0000

    test for rat prepared

---
+++ specs/rat_spec.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const Rat = require('../rat.js');
+
+describe('rat', function() {
+ let rat;
+
+ beforeEach(function() {
+  rat = new rat();
+ });
+
+ it('should be able to touch food', function() {
+ assert.strictEqual(task.difficulty, 1);
+ });
+});
