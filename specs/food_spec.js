45ffbfa3ee94504bbc610c8a623e0a0e2e3449da
Huascar <huascarmc@hotmail.com>
2017-12-08 13:53:08 +0000

    added test for food

---
+++ specs/food_spec.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const Food = require('../food.js')
+
+describe('food', function() {
+ food = new Food('coconut', 100);
+
+ it('has a name', function() {
+  assert.strictEqual(food.coconut, 'coconut');
+ });
+
+ it('has a replenishment value', function() {
+  assert.strictEqual(food.replenishment, 100);
+ })
+});
