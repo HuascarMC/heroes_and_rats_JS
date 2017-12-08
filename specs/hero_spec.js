03e9f3d6630684e4e9e1c78db6ca12fe6b69ede3
Huascar <huascarmc@hotmail.com>
2017-12-08 13:22:53 +0000

    added test for hero

---
+++ specs/hero_spec.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const Hero = require('../hero.js');
+
+describe('hero', function() {
+ let hero;
+
+ beforeEach(function() {
+  hero = new Hero('Jack Sparrow', 100, 'Rum');
+ });
+
+ 
+})
