738c72411e95f8d3003cb984c04eb5fee93f8d7f
Huascar <huascarmc@hotmail.com>
2017-12-08 13:48:24 +0000

    added constructor for task class ,tests passing

---
+++ task.js
@@ -0,0 +1,8 @@
+const Task = function(difficulty, urgency, reward, completed) {
+ this.difficulty = difficulty;
+ this.urgency = urgency;
+ this.reward = reward;
+ this.completed = completed;
+}
+
+module.exports = Task;
