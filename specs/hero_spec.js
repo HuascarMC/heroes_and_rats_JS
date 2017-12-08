77588bf951784f4c74d96eb6184d51d2f0996a0a
Huascar <huascarmc@hotmail.com>
2017-12-08 13:57:50 +0000

    added test for hero to eat, required files for test

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,11 +1,15 @@
 const assert = require('assert');
 const Hero = require('../hero.js');
+const Food = require('../food.js');
+const Task = require('../task.js');
 
 describe('hero', function() {
  let hero;
 
  beforeEach(function() {
-  hero = new Hero('Jack Sparrow', 100, 'Rum');
+  hero = new Hero('Jack Sparrow', 90, 'Rum');
+  food = new Food('coconut', 100);
+  task = new Task(1, 2, 3, false);
  });
 
  it('should have a name', function() {
@@ -13,10 +17,15 @@ describe('hero', function() {
  });
 
  it('should have health', function() {
-  assert.strictEqual(hero.health, 100);
+  assert.strictEqual(hero.health, 90);
  });
 
  it('should have a favourite food', function() {
   assert.strictEqual(hero.favFood, 'Rum');
  });
+
+ it('should be able to eat food', function() {
+  hero.eat(food);
+  assert.strictEqual(hero.health, 100);
+ })
 });
